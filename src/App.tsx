dc53504b15c72ebea8e258ea6c0457b51f458ac4
Tofail Ahmed Sayem <atofail50@gmail.com>
2024-01-22 00:15:04 +0600

    stagger added

--- src/App.tsx
+++ src/App.tsx
@@ -2,17 +2,31 @@ import "./App.css";
 import { motion } from "framer-motion";
 
 function App() {
-  const initial={ rotate: 0,scale:1 }
-  const animate={ rotate: 360,scale:1.5 }
-  const transition={ duration: 2, repeat: Infinity,type:"spring"}
+ 
+
+  const parent={
+     initial:{ opacity: 0,scale:.7 },
+     animate:{ opacity: 1,scale:1.3 }
+  }
+  const child={
+    initial:{ opacity: 0,scale:.5 },
+     animate:{ opacity: 1,scale:1 }
+  }
   return (
     <div className="flex justify-center items-center h-screen box-parent">
       <motion.div
-        initial={initial}
-        animate={animate}
-        transition={transition}
-        className=" box"
-      ></motion.div>
+      variants={parent}
+        initial="initial"
+        animate="animate"
+        transition= {{ duration: 2, type:"spring",repeatType:"reverse",delayChildren:0.5,staggerChildren:0.5}}
+
+        className=" box flex flex-wrap gap-4 justify-center items-center"
+      >
+        <motion.div variants={child} className="size-20 bg-amber-500 rounded-sm"></motion.div>
+        <motion.div variants={child} className="size-20 bg-amber-500 rounded-sm"></motion.div>
+        <motion.div variants={child} className="size-20 bg-amber-500 rounded-sm"></motion.div>
+        <motion.div variants={child} className="size-20 bg-amber-500 rounded-sm"></motion.div>
+      </motion.div>
     </div>
   );
 }
